7cda4f7b31c5701f44c544cbd082495ad41c06a5
zekeriyasari <zekeriya.sari.deu@gmail.com>
2017-10-06 11:15:21 +0300

    Included an animated plot to test the interpolation

--- test/test_interpolation.jl
+++ test/test_interpolation.jl
@@ -140,12 +140,21 @@ f = spline_interpolate(x, y, spline_type="cubic")
 
 fig = figure()
 ax = fig[:add_subplot](111)
-ax[:plot](x, y, ".")
-curve, = ax[:plot]([], [])
+ax[:plot](x, y, ".", label="Interpolation Data")
+ax[:set_xlabel]("x")
+ax[:set_ylabel]("y")
+x_data = []
+y_data = []
+curve, = ax[:plot]([], [], label="Cubic Spline Interpolation")
+ax[:legend]()
+ax[:set_title]("Cubic spline interplation to f(x) = 1 / (1 + x^2) sin(x)")
 for xi in x
-    curve[:set_xdata](xi)
-    curve[:set_ydata](f(xi))
-    fig[:canvas][:draw]()
+    x_data = [x_data; xi]
+    y_data = [y_data; f(xi)]
+    curve[:set_xdata](x_data)
+    curve[:set_ydata](y_data)
     fig[:canvas][:flush_events]()
+    ax[:relim]()
+    sleep(0.25)
 end
 println("Passed...\n")
